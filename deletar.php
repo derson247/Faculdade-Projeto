847003368bfef87d1bcdeba17d6f0a3a9d341265
pedrook80 <50674601+pedrook80@users.noreply.github.com>
2020-06-15 11:42:06 -0300

    Add files via upload

---
+++ deletar.php
@@ -0,0 +1,20 @@
+<?php 
+
+session_start();
+echo $id=$_POST["id_usuarios"];
+
+
+$connect = mysqli_connect('sql10.freesqldatabase.com','sql10346904','PqmNhtNC6g','sql10346904', '3306');
+$ver = "DELETE from usuarios WHERE id=$id";
+$verifica = mysqli_query($connect,$ver);
+//$dados = mysqli_fetch_assoc($verifica);
+  
+session_destroy();
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Usuario Deletado');window.location
+        .href='index.php';</script>";   
+
+         
+      
+?>
